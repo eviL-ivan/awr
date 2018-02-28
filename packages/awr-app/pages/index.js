84097d36fc14bf5ff6bc15b5cfb4944c791e67b9
mxdroni <mxdroni@mail.ru>
2018-02-28 18:03:58 +0300

    Update index.js

--- packages/awr-app/pages/index.js
+++ packages/awr-app/pages/index.js
@@ -2,17 +2,26 @@ import React from "react";
 import Page from "components/Layout/Page";
 import withLayout from "utils/withLayout";
 import withStoreProvider from "utils/withStoreProvider";
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import Link from "next/link";
+import Button from "material-ui/Button";
 
 const Main = styled.div`
-    width:30%;
-    margin: 100 auto;
+  width: 30%;
+  margin: 100 auto;
 `;
 
 @withLayout
 @withStoreProvider
 export default class Counter extends React.Component {
   render() {
-    return <Main>Index1</Main>;
+    return (
+      <Main>
+        <Link href={{ pathname: "/report", query: { id: 1 } }}>
+          <Button>Отчет</Button>
+        </Link>
+      </Main>
+    );
   }
 }
